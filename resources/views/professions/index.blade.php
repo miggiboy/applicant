d205c91a0ea2f27a53bf0a19e0b5ff36deb05e7d
miggiboy <miras.mustimov@tuta.io>
2017-07-20 13:58:47 +0000

    professions.index view bugs fixed

--- resources/views/professions/index.blade.php
+++ resources/views/professions/index.blade.php
@@ -6,20 +6,18 @@
 
 @section('content')
   <div id="subpage">
-    <h1>Профессии по направлению "{{ $category->title }}"</h1>
+    <h1><a href="{{ url('/professions/categories') }}">Профессии</a> по направлению "{{ $category->title }}"</h1>
     <hr size="1" color="#ff831f">
     <hr size="1" color="#ff5500">
     <hr size="1" color="#ffb47a">
     @if (count($professions))
       <div class="ui large celled very relaxed selection list">
           @foreach ($professions as $profession)
-          <div class="university item" style="cursor: default;">
-              <div class="ui right pointing right floated icon dropdown basic button content" style="left: 0px; width: 940px;">
-                  <div class="content">
-                      <a class="header" href="{{ route('professions.show', $profession) }}">
-                        <i class="teal student icon"></i>{{$profession->title}}
-                      </a>
-                  </div>
+          <div class="item" style="cursor: default;">
+              <div class="content">
+                  <a class="header" href="{{ route('professions.show', $profession) }}">
+                    <i class="teal student icon"></i>{{$profession->title}}
+                  </a>
               </div>
           </div>
           @endforeach
